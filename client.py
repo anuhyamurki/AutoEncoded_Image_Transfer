3eba5b2beec3727d4a7adf8fe0fa966291943f62
Cioraz <sunilthunga.221cs252@nitk.edu.in>
2024-01-11 23:56:46 +0530

    Added functionality for opening any file using tk

--- client.py
+++ client.py
@@ -1,15 +1,32 @@
-import socket 
+import socket
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import time
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost',1234))
+def receive_image_client(server_ip, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
 
-file = open('client.jpeg','rb')
+    image_data = b""
+    while True:
+        chunk = client_socket.recv(1024)
+        if not chunk:
+            break
+        image_data += chunk
 
-image_data = file.read(2048)
+    client_socket.close()
 
-while image_data:
-    client.send(image_data)
-    image_data = file.read(2048)
+    display_received_image(image_data)
 
-file.close()
-client.close()
+def display_received_image(image_data):
+    image = Image.open(BytesIO(image_data))
+    plt.imshow(image)
+    plt.title("Received Image")
+    plt.show()
+
+if __name__ == "__main__":
+    server_ip = '127.0.0.1' # Keep the server IP here
+    server_port = 55555 # Random port which is free at any time
+
+    receive_image_client(server_ip, server_port)
