c6ea89124aad98a6fd4deabd18a868c08f548dd6
Aryan Kashyap Naveen <120780494+05kashyap@users.noreply.github.com>
2024-01-17 23:25:59 +0530

    Added encoder-decoder functionality to the client, server scripts.

---
+++ client_decoder.py
@@ -0,0 +1,77 @@
+import socket
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import torch
+import time
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import io
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        # Decoder layers
+        ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+        self.t_conv1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.t_conv3 = nn.ConvTranspose2d(64, 3, kernel_size=2, stride=2)
+
+
+    def forward(self, x_encoded):
+        x = F.relu(self.t_conv1(x_encoded))
+        x = F.relu(self.t_conv2(x))
+        x = F.sigmoid(self.t_conv3(x))
+
+        return x
+
+
+decoder = Decoder()
+decoder.load_state_dict(torch.load('Models\decoder_model.pth',map_location=torch.device('cpu')))
+decoder.eval()
+print(decoder)
+
+def receive_image_client(server_ip, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+
+    start_time = time.time()
+
+    image_data = b""
+    while True:
+        chunk = client_socket.recv(1024)
+        if not chunk:
+            break
+        image_data += chunk
+    received_encoded_output = torch.load(io.BytesIO(image_data))
+    end_time = time.time()
+    transfer_time = end_time - start_time
+
+    print(f"Image received successfully in {transfer_time} seconds")
+    client_socket.close()
+    
+    display_received_image(received_encoded_output)
+
+def display_received_image(encoded_output):
+    #result=torch.Tensor(numpy.frombuffer(encoded_output, dtype=numpy.int32))
+    decoded_output = decoder(encoded_output)
+    #image = Image.open(BytesIO(decoded_output))
+    decoded_output_np = decoded_output.detach().numpy()
+    img = np.transpose(decoded_output_np[0], (1, 2, 0))
+    
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.imshow(img)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+    plt.title('recieved image')
+    plt.show()
+   
+
+if __name__ == "__main__":
+    server_ip = '192.168.56.1' # Keep the server IP here
+    server_port = 55555 # Random port which is free at any time
+
+    receive_image_client(server_ip, server_port)
---
+++ server_encoder.py
@@ -0,0 +1,94 @@
+import socket
+import tkinter as tk
+from tkinter import filedialog
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2 as cv
+from PIL import Image
+import io
+import torchvision.transforms as transforms
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+        # Encoder layers
+         # conv layer (depth from 3 --> 64), 3x3 kernels
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+         # conv layer (depth from 3 --> 16), 3x3 kernels
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+         # conv layer (depth from 3 --> 16), 3x3 kernels
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+
+        # Encoder
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x_encoded = self.pool(x)
+        return x_encoded
+
+
+encoder = Encoder()
+encoder.load_state_dict(torch.load('Models\encoder_model.pth', map_location=torch.device('cpu')))
+encoder.eval()
+
+def send_image_server(ip, port, image_path):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip, port))
+    server_socket.listen(1)
+
+    print(f"Server listening on {ip}:{port}")
+
+    while True:
+        data_connection, address = server_socket.accept()
+        print(f"Connection from {address}")
+
+        start_time = time.time()    
+        with open(image_path, 'rb') as file:
+            image_data = file.read()
+            pil_image = Image.open(io.BytesIO(image_data))
+
+        transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        ])
+
+        # Convert the resized image to a tensor
+        pil_image = pil_image.convert('RGB')
+
+        image_tensor = transform(pil_image).unsqueeze(0)
+        # Pass the resized image tensor to the encoder
+        encoded_output = encoder(image_tensor)
+        buffer = io.BytesIO()
+        torch.save(encoded_output, buffer)
+        encoded_output_bytes = buffer.getvalue()
+
+        # Now you can send `encoded_output_bytes` over the network connection
+        # For example, assuming `data_connection` is a socket
+        data_connection.sendall(encoded_output_bytes)
+        data_connection.close()
+
+        end_time = time.time()
+        transfer_time = end_time - start_time
+
+        print(f"Image has sent successfully in {transfer_time}")
+
+if __name__ == "__main__":
+    server_ip = '192.168.56.1' # Keep the server ip
+    server_port = 55555 # any random always free port
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    print(file_path)
+    image_to_send = file_path  # Replace with the actual image path
+
+    send_image_server(server_ip, server_port, image_to_send)
